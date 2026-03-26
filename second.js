c4951d85e8f738ffdeb0ec2b7cbeac8b8d24cd94
Agnes Eriksson <agnes.eriksson.01@gmail.com>
2026-03-26 09:47:19 +0100

    Inint commit ssecond js for lists

---
+++ second.js
@@ -0,0 +1,39 @@
+//-------------------------------------------
+const books = [
+    {
+        image: "thumbnail-sower.jpg",
+        title: "Liknelsen om Sådden",
+        author: "Butler, Octavia",
+        published: 2019,
+        form: "Bok, fysiskt material"
+    },
+    {
+        image: "thumbnail-died.jpg",
+        title: "The Man Who Died Twice",
+        author: "Osman, Richard",
+        published: 2021,
+        form: "Bok, fysiskt material"
+    },
+    {
+        image: "thumbnail-crime.jpg",
+        title: "Crime and Punsihment",
+        author: "Dostojevskij, Fjodor",
+        published: 2018,
+        form: "Bok, fysiskt material"
+    },
+    {
+        image: "thumbnail-sea.jpg",
+        title: "Somewhere Beyond the Sea",
+        author: "Klune, TJ",
+        published: 2024,
+        form: "Bok, fysiskt material"
+    }
+];
+
+
+
+//---VIKTIGT---
+// - Reservera från minneslistan ("vill du ta bort från listan?")
+// - Ta bort från minneslistan
+// - Fälla ut mer info till böckerna
+// - Fixa filter, sortera
